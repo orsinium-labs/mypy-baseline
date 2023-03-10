d5f012c669d31cc1b6d557358cf605c5be57d706
gram <git@orsinium.dev>
2023-03-10 10:10:08 +0100

    better gitlab comment detection

--- mypy_baseline/commands/_suggest.py
+++ mypy_baseline/commands/_suggest.py
@@ -226,7 +226,7 @@ class Suggest(Command):
         import requests
 
         resp = requests.get(
-            url=self._gitlab_comment_url,
+            url=f'{self._gitlab_comment_url}?sort=asc&per_page=100',
             headers={'PRIVATE-TOKEN': token},
         )
         resp.raise_for_status()
