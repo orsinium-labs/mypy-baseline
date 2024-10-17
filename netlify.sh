4a4f09dae7dc560424cc6ba8b11f99cc14d0a3c8
gram <git@orsinium.dev>
2024-10-17 10:22:45 +0200

    update sphinx

--- netlify.sh
+++ netlify.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # This script is used by netlify to build docs
 set -e
-python3 -m pip install '.[docs]'
+python3 -m pip install --break-system-packages '.[docs]'
 cp -r assets docs/
-sphinx-build docs docs/build
+python3 -m sphinx docs docs/build
